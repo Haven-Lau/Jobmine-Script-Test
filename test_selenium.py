586cf9f78e605f2236c960213d2714b80bc4b765
Ernest Wong <chewong@uwaterloo.ca>
2016-05-16 22:58:44 -0400

    Able to choose discipline

--- test_selenium.py
+++ test_selenium.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import time
 
 USERNAME = ""
 PASSWORD = ""
+discipline_1 = "ENG-Computer"
+discipline_2 = "ENG-Software"
+discipline_3 = "MATH-Computer Science"
+discipline_list = [discipline_1, discipline_2, discipline_3]
 
 browser = webdriver.Firefox()
 
@@ -30,17 +35,20 @@ browser.get('https://jobmine.ccol.uwaterloo.ca/psp/SS/EMPLOYEE/WORK/c/UW_CO_STUD
 
 browser.switch_to.frame(browser.find_element_by_id("ptifrmtgtframe"))
 
+# Choose disciplines
+for x in xrange(0,3):
+    select_discipline = Select(browser.find_element_by_id('UW_CO_JOBSRCH_UW_CO_ADV_DISCP%d' % (x + 1)))
+    select_discipline.select_by_visible_text(discipline_list[x])
+
 searchbtn = browser.find_element_by_xpath('//*[@id="UW_CO_JOBSRCHDW_UW_CO_DW_SRCHBTN"]')
 searchbtn.click()
 
-element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "UW_CO_JOBTITLE_HL$1"))
-    	)
+element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "UW_CO_JOBTITLE_HL$1")))
 
-for inc in range(0,4):
-	jobtitle_id = 'UW_CO_JOBTITLE_HL$%d'%(inc)
-	jobtitle = browser.find_element_by_id(jobtitle_id)
-	time.sleep(1)
-	jobtitle.click()
+for inc in xrange(0,4):
+    jobtitle_id = 'UW_CO_JOBTITLE_HL$%d' % (inc)
+    jobtitle = browser.find_element_by_id(jobtitle_id)
+    time.sleep(1)
+    jobtitle.click()
 
 browser.switch_to_default_content()
